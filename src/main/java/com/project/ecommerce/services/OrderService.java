e4f9dced1963e0c5369fb8ba44b90e669f43ff89
CemTheCoder <ccem564@gmail.com>
2023-02-04 13:01:23 +0300

    order

--- src/main/java/com/project/ecommerce/services/OrderService.java
+++ src/main/java/com/project/ecommerce/services/OrderService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.ecommerce.dao.CartDao;
+import com.project.ecommerce.dao.CartItemDao;
 import com.project.ecommerce.dao.OrderDao;
 import com.project.ecommerce.dao.UserDao;
+import com.project.ecommerce.entities.Cart;
 import com.project.ecommerce.entities.Order;
 import com.project.ecommerce.entities.User;
 import com.project.ecommerce.requests.OrderRequest;
@@ -17,6 +20,11 @@ public class OrderService {
 	@Autowired
 	private OrderDao dao;
 
+	@Autowired
+	private CartItemDao cartItemDao;
+	
+	@Autowired
+	private CartDao cartDao;
 	
 	
 	public Order create(OrderRequest r) {
@@ -25,6 +33,10 @@ public class OrderService {
 		o.setAddress(r.getAddress());
 		o.setUser(r.getUser());
 		o.setPrice(r.getPrice());
+		this.cartItemDao.deleteAll();
+		Cart cart = this.cartDao.findByUserId(r.getUser().getId());
+		cart.setTotal(0);
+		this.cartDao.save(cart);
 		return this.dao.save(o);
 	}
 	
